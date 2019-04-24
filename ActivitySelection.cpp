9a091e85a0fc8d6ba732aa4b0734eb281856da07
gourisnair <gouri.gopan@gmail.com>
2019-04-25 05:27:29 +0530

    Actiivty selection using greedy

--- ActivitySelection.cpp
+++ ActivitySelection.cpp
@@ -1,24 +1,27 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-  int s[10], f[10], n, i;
-  s[0] = f[0] = 0;
-  cout << "Enter the number of activities: ";
-  cin >> n;
-  cout << "Enter the starting and ending time: ";
-  for(i = 1; i <= n; i++) {
-    cin >> s[i];
-    cin >> f[i];
-  }
-  i = 0;
-
-  cout << "The selected activities are: ";
-  cout << i;
-  for(int j = 2;j <= n; j++) {
-    if(s[j] >= f[i]) {
-      cout << j;
-      i = j;
-    }
-  }
+int main(){
+	int n,s[20],f[20];
+	cout<<"Enter the total number of activities: ";
+	cin>>n;
+	cout<<"Enter the start time and finish time of "<<n<<" activities\n";
+	s[0]=f[0]=0;
+	for(int i=1;i<=n;i++)
+	{
+		cin>>s[i];
+		cin>>f[i];
+	}
+	int i, j;
+	cout<<"Following activities are selected: ";
+	i = 1;
+	cout<<i;
+	for (j = 2; j<=n; j++)
+    	{
+      		if (s[j] >= f[i])
+      		{
+          		cout<<j;
+          		i = j;
+      		}
+    	}
+	return 0;
 }
